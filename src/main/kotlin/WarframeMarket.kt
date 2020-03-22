8ce145c14b353b11171763e4fa5d61fc4e12f39b
LastExceed <chris20194@googlemail.com>
2020-03-22 21:25:30 +0100

    add v1 object

--- src/main/kotlin/WarframeMarket.kt
+++ src/main/kotlin/WarframeMarket.kt
@@ -46,122 +46,123 @@ object WarframeMarket : Endpoint(null) {
 			}
 		}
 
-	object auctions : Endpoint(WarframeMarket), Get<Auctions> {
-		override suspend fun get() = httpClient.get<PayloadContainer<Auctions>>(url).payload
-	}
-
-	object auth : Endpoint(WarframeMarket) {
-		suspend fun signIn(payload: Credentials) {
-			httpClient.post<PayloadContainer<ProfilePrivate>>(createRequestBuilder("$url/signin", payload)).payload
-			jwt = httpClient.cookies("$url/signin").find { it.name == "JWT" }!!.value
+	object v1 : Endpoint(WarframeMarket) {
+		object auctions : Endpoint(WarframeMarket), Get<Auctions> {
+			override suspend fun get() = httpClient.get<PayloadContainer<Auctions>>(url).payload
 		}
-		suspend fun signOut(): Unit { //returns a profile with all values null
-			httpClient.get<HttpResponse>("$url/signout")
-			jwt = ""
+
+		object auth : Endpoint(WarframeMarket) {
+			suspend fun signIn(payload: Credentials) {
+				httpClient.post<PayloadContainer<ProfilePrivate>>(createRequestBuilder("$url/signin", payload)).payload
+				jwt = httpClient.cookies("$url/signin").find { it.name == "JWT" }!!.value
+			}
+			suspend fun signOut(): Unit { //returns a profile with all values null
+				httpClient.get<HttpResponse>("$url/signout")
+				jwt = ""
+			}
+			suspend fun register(): Nothing = TODO()
 		}
-		suspend fun register(): Nothing = TODO()
-	}
 
-	object items : Endpoint(WarframeMarket), Get<Items> {
-		override suspend fun get() = httpClient.get<PayloadContainer<Items>>(createRequestBuilder(url)).payload
+		object items : Endpoint(WarframeMarket), Get<Items> {
+			override suspend fun get() = httpClient.get<PayloadContainer<Items>>(createRequestBuilder(url)).payload
 
-		class URL_NAME(override val pathName: String) : Endpoint(items), Get<Item> {
-			override suspend fun get() = httpClient.get<PayloadContainer<Item>>(createRequestBuilder(url)).payload
+			class URL_NAME(override val pathName: String) : Endpoint(items), Get<Item> {
+				override suspend fun get() = httpClient.get<PayloadContainer<Item>>(createRequestBuilder(url)).payload
 
-			val orders = Orders(this)
-			val statistics = Statistics(this)
+				val orders = Orders(this)
+				val statistics = Statistics(this)
 
-			class Orders(parent: Endpoint?) : Endpoint(parent), Get<ItemOrders> {
-				override suspend fun get() = httpClient.get<PayloadContainer<ItemOrders>>(createRequestBuilder(url)).payload
+				class Orders(parent: Endpoint?) : Endpoint(parent), Get<ItemOrders> {
+					override suspend fun get() = httpClient.get<PayloadContainer<ItemOrders>>(createRequestBuilder(url)).payload
 
-				val top = Top(this)
+					val top = Top(this)
 
-				class Top(parent: Endpoint?) : Endpoint(parent), Get<ItemOrdersTop> {
-					override suspend fun get() = httpClient.get<PayloadContainer<ItemOrdersTop>>(createRequestBuilder(url)).payload
+					class Top(parent: Endpoint?) : Endpoint(parent), Get<ItemOrdersTop> {
+						override suspend fun get() = httpClient.get<PayloadContainer<ItemOrdersTop>>(createRequestBuilder(url)).payload
+					}
+				}
+
+				class Statistics(parent: Endpoint?) : Endpoint(parent), Get<ItemStatistics> {
+					override suspend fun get() = httpClient.get<PayloadContainer<ItemStatistics>>(createRequestBuilder(url)).payload
+				}
+			}
+		}
+
+		object tools : Endpoint(WarframeMarket) {
+			object ducats : Endpoint(tools), Get<Ducanator> {
+				override suspend fun get() = httpClient.get<PayloadContainer<Ducanator>>(createRequestBuilder(url)).payload
+			}
+		}
+
+		object profile : Endpoint(WarframeMarket), Get<ProfilePrivate> {
+			override suspend fun get() = httpClient.get<PayloadContainer<ProfilePrivate>>(createRequestBuilder(url)).payload
+
+			object customization : Endpoint(profile) {
+				object about : Endpoint(customization), Update<AboutUpdate, About> {
+					override suspend fun update(payload: AboutUpdate) = httpClient.put<PayloadContainer<About>>(createRequestBuilder(url, payload)).payload
+				}
+
+				object avatar : Endpoint(customization), Create<Nothing, ProfilePrivate> {
+					override suspend fun create(payload: Nothing) = TODO("not implemented")
 				}
 			}
 
-			class Statistics(parent: Endpoint?) : Endpoint(parent), Get<ItemStatistics> {
-				override suspend fun get() = httpClient.get<PayloadContainer<ItemStatistics>>(createRequestBuilder(url)).payload
-			}
-		}
-	}
+			object orders : Endpoint(profile), Get<OwnOrders>, Create<OrderCreation, Order> {
+				override suspend fun get() = httpClient.get<PayloadContainer<OwnOrders>>(createRequestBuilder(url)).payload
 
-	object tools : Endpoint(WarframeMarket) {
-		object ducats : Endpoint(tools), Get<Ducanator> {
-			override suspend fun get() = httpClient.get<PayloadContainer<Ducanator>>(createRequestBuilder(url)).payload
-		}
-	}
+				override suspend fun create(payload: OrderCreation) = httpClient.post<PayloadContainer<Order>>(createRequestBuilder(url, payload)).payload
 
-	object profile : Endpoint(WarframeMarket), Get<ProfilePrivate> {
-		override suspend fun get() = httpClient.get<PayloadContainer<ProfilePrivate>>(createRequestBuilder(url)).payload
+				class ORDER_ID(override val pathName: String) : Endpoint(orders), Update<OrderUpdate, Order>, Delete<OrderDeletionConfirmation> {
+					override suspend fun update(payload: OrderUpdate) = httpClient.put<PayloadContainer<Order>>(createRequestBuilder(url, payload)).payload
 
-		object customization : Endpoint(profile) {
-			object about : Endpoint(customization), Update<AboutUpdate, About> {
-				override suspend fun update(payload: AboutUpdate) = httpClient.put<PayloadContainer<About>>(createRequestBuilder(url, payload)).payload
-			}
+					override suspend fun delete() = httpClient.delete<PayloadContainer<OrderDeletionConfirmation>>(createRequestBuilder(url)).payload
 
-			object avatar : Endpoint(customization), Create<Nothing, ProfilePrivate> {
-				override suspend fun create(payload: Nothing) = TODO("not implemented")
-			}
-		}
-
-		object orders : Endpoint(profile), Get<OwnOrders>, Create<OrderCreation, Order> {
-			override suspend fun get() = httpClient.get<PayloadContainer<OwnOrders>>(createRequestBuilder(url)).payload
-
-			override suspend fun create(payload: OrderCreation) = httpClient.post<PayloadContainer<Order>>(createRequestBuilder(url, payload)).payload
-
-			class ORDER_ID(override val pathName: String) : Endpoint(orders), Update<OrderUpdate, Order>, Delete<OrderDeletionConfirmation> {
-				override suspend fun update(payload: OrderUpdate) = httpClient.put<PayloadContainer<Order>>(createRequestBuilder(url, payload)).payload
-
-				override suspend fun delete() = httpClient.delete<PayloadContainer<OrderDeletionConfirmation>>(createRequestBuilder(url)).payload
-
-				//this one has a weird url, therefore implemented manually
-				suspend fun close() = httpClient.put<PayloadContainer<OrderCloseConfirmation>>(createRequestBuilder(orders.url + "/close/" + pathName)).payload
-			}
-		}
-
-		class USERNAME(override val pathName: String) : Endpoint(profile), Get<ProfilePublic> {
-			override suspend fun get() = httpClient.get<PayloadContainer<ProfilePublic>>(createRequestBuilder(url)).payload
-
-			val achievements = Achievements(this)
-			val orders = Orders(this)
-			val review = Review(this)
-			val reviews = Reviews(this)
-			val statistics = Statistics(this)
-
-			class Achievements(parent: Endpoint?) : Endpoint(parent), Get<UserAchievements> {
-				override suspend fun get() = httpClient.get<PayloadContainer<UserAchievements>>(createRequestBuilder(url)).payload
-			}
-
-			class Orders(parent: Endpoint?) : Endpoint(parent), Get<UserOrders> {
-				override suspend fun get() = httpClient.get<PayloadContainer<UserOrders>>(createRequestBuilder(url)).payload
-			}
-
-			class Review(parent: Endpoint?) : Endpoint(parent), Create<ReviewCreation, ReviewCreationConfirmation> {
-				override suspend fun create(payload: ReviewCreation) = httpClient.post<PayloadContainer<ReviewCreationConfirmation>>(createRequestBuilder(url, payload)).payload
-				
-				fun REVIEW_ID(review_id: String) = REVIEW_ID(review_id, this)
-
-				class REVIEW_ID(override val pathName: String, parent: Endpoint?) : Endpoint(parent), Update<ReviewUpdate, ReviewUpdateConfirmation>, Delete<ReviewDeletionConfirmation> {
-					override suspend fun update(payload: ReviewUpdate) = httpClient.put<PayloadContainer<ReviewUpdateConfirmation>>(createRequestBuilder(url, payload)).payload
-
-					override suspend fun delete() = httpClient.delete<PayloadContainer<ReviewDeletionConfirmation>>(createRequestBuilder(url)).payload
+					//this one has a weird url, therefore implemented manually
+					suspend fun close() = httpClient.put<PayloadContainer<OrderCloseConfirmation>>(createRequestBuilder(orders.url + "/close/" + pathName)).payload
 				}
 			}
 
-			class Reviews(parent: Endpoint?) : Endpoint(parent), Get<Reviews> {
-				override suspend fun get() = httpClient.get<PayloadContainer<Reviews>>(createRequestBuilder(url)).payload
-			}
+			class USERNAME(override val pathName: String) : Endpoint(profile), Get<ProfilePublic> {
+				override suspend fun get() = httpClient.get<PayloadContainer<ProfilePublic>>(createRequestBuilder(url)).payload
 
-			class Statistics(parent: Endpoint?) : Endpoint(parent), Get<UserStatistics> {
-				override suspend fun get() = httpClient.get<PayloadContainer<UserStatistics>>(createRequestBuilder(url)).payload
+				val achievements = Achievements(this)
+				val orders = Orders(this)
+				val review = Review(this)
+				val reviews = Reviews(this)
+				val statistics = Statistics(this)
+
+				class Achievements(parent: Endpoint?) : Endpoint(parent), Get<UserAchievements> {
+					override suspend fun get() = httpClient.get<PayloadContainer<UserAchievements>>(createRequestBuilder(url)).payload
+				}
+
+				class Orders(parent: Endpoint?) : Endpoint(parent), Get<UserOrders> {
+					override suspend fun get() = httpClient.get<PayloadContainer<UserOrders>>(createRequestBuilder(url)).payload
+				}
+
+				class Review(parent: Endpoint?) : Endpoint(parent), Create<ReviewCreation, ReviewCreationConfirmation> {
+					override suspend fun create(payload: ReviewCreation) = httpClient.post<PayloadContainer<ReviewCreationConfirmation>>(createRequestBuilder(url, payload)).payload
+
+					fun REVIEW_ID(review_id: String) = REVIEW_ID(review_id, this)
+
+					class REVIEW_ID(override val pathName: String, parent: Endpoint?) : Endpoint(parent), Update<ReviewUpdate, ReviewUpdateConfirmation>, Delete<ReviewDeletionConfirmation> {
+						override suspend fun update(payload: ReviewUpdate) = httpClient.put<PayloadContainer<ReviewUpdateConfirmation>>(createRequestBuilder(url, payload)).payload
+
+						override suspend fun delete() = httpClient.delete<PayloadContainer<ReviewDeletionConfirmation>>(createRequestBuilder(url)).payload
+					}
+				}
+
+				class Reviews(parent: Endpoint?) : Endpoint(parent), Get<Reviews> {
+					override suspend fun get() = httpClient.get<PayloadContainer<Reviews>>(createRequestBuilder(url)).payload
+				}
+
+				class Statistics(parent: Endpoint?) : Endpoint(parent), Get<UserStatistics> {
+					override suspend fun get() = httpClient.get<PayloadContainer<UserStatistics>>(createRequestBuilder(url)).payload
+				}
 			}
 		}
-	}
 
-	object settings : Endpoint(WarframeMarket) {
-		//TODO
+		object settings : Endpoint(WarframeMarket) {
+			//TODO
 //		object accounts : Endpoint(settings), Get<ResponsePayload>, Update<RequestPayload, ResponsePayload> {
 //			override suspend fun get() = httpClient.get<PayloadContainer<ResponsePayload>>(createRequestBuilder(url)).payload
 //			override suspend fun update(payload: RequestPayload) = httpClient.put<PayloadContainer<ResponsePayload>>(createRequestBuilder(url, payload)).payload
@@ -190,9 +191,10 @@ object WarframeMarket : Endpoint(null) {
 //			override suspend fun get() = httpClient.get<PayloadContainer<ResponsePayload>>(createRequestBuilder(url)).payload
 //			//TODO: patch
 //		}
-	}
+		}
 
-	object statistics : Endpoint(WarframeMarket), Get<Nothing> {
-		override suspend fun get() = error("this endpoint is currently out of service. Blame KycKyc")
+		object statistics : Endpoint(WarframeMarket), Get<Nothing> {
+			override suspend fun get() = error("this endpoint is currently out of service. Blame KycKyc")
+		}
 	}
 }
