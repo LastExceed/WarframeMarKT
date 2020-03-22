e3172a41a2ea5ef7a24ccc1f982f2baf544df05d
LastExceed <chris20194@googlemail.com>
2020-03-22 21:48:48 +0100

    cleanup main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,25 +1,10 @@
 suspend fun main() {
-	try {
-		val x = WarframeMarket.items.URL_NAME("maiming_strike").get()
-		println("success")
-		println(x)
-	} catch (ex: Exception) {
-		println("error")
-		println(ex.message)
-	}
-
 }
 
 //TODO:
 //- accessibility modifiers
-//  - private constructors
-//- rename variables using annotations
 //- handle null values in payloads explicitly
 //- rename confirmation payloads
 
 //POST customization omitted because undocumented
-
 //dont use type hierachy for return types because of recurring return types
-//GET single order doesnt exist anymore
-//GET settings moved to settings/verification
-// [close order] is  profile/orders/close/{ID}  instead of  profile/orders/{ID}/close
