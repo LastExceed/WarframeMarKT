2b4629529359c1386203dddc4e4f3d2d1aa4e566
LastExceed <chris20194@googlemail.com>
2020-03-24 13:08:24 +0100

    delete main.kt

--- src/main/kotlin/main.kt
+++
@@ -1,7 +0,0 @@
-suspend fun main() {
-}
-
-//TODO:
-//implement include
-//check if kotlin 1.4 fixes type erasure
-//getting reviews without auth breaks because for some reason it includes the personal profile which is full of nulls when not logged in
