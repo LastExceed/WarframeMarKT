1fc524a11ab9824e852a074da661b4a06480a250
LastExceed <chris20194@googlemail.com>
2020-03-24 12:57:45 +0100

    add missing riven attributes

--- src/main/kotlin/enums/RivenAttribute.kt
+++ src/main/kotlin/enums/RivenAttribute.kt
@@ -1,8 +1,38 @@
 package enums
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@Suppress("EnumEntryName")
 enum class RivenAttribute {
+	@JsonProperty("base_damage_/_melee_damage")base_damage_OR_melee_damage,
+	@JsonProperty("fire_rate_/_attack_speed")`fire_rate_OR_attack_speed`,
+	ammo_maximum,
+	chance_to_gain_extra_combo_count,
+	channeling_damage,
+	channeling_efficiency,
 	cold_damage,
-	damage_vs_corpus,
+	combo_duration,
 	critical_chance,
-	magazine_capacity
+	critical_chance_on_slide_attack,
+	critical_damage,
+	damage_vs_corpus,
+	damage_vs_grineer,
+	damage_vs_infested,
+	electric_damage,
+	finisher_damage,
+	heat_damage,
+	impact_damage,
+	magazine_capacity,
+	multishot,
+	projectile_speed,
+	punch_through,
+	puncture_damage,
+	range,
+	recoil,
+	reload_speed,
+	slash_damage,
+	status_chance,
+	status_duration,
+	toxin_damage,
+	zoom,
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,27 +1,4 @@
-import payload.request.SigninCredentials
-
-typealias mx = Int
-typealias my = Int
-
 suspend fun main() {
-	try {
-		val profile = WarframeMarket.v1.auth.signIn(
-			SigninCredentials(
-				"chris20194@googlemail.com",
-				"pIemCfgZ5ym7^6!Xmqhf5@kNuGR@tkvr77GA9\$fE"
-			)
-		)
-		println("ok")
-
-		val x = WarframeMarket.v1.items.get()
-		println(x.items.size)
-		val y = WarframeMarket.v1.profile.USER("LastExceed").get()
-		println(y.profile.last_seen)
-	} catch (ex: Exception) {
-		println(ex.message)
-	}
-	println("done")
-	readLine()
 }
 
 //TODO:
