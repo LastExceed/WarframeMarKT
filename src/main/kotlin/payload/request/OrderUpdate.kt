dbd28514535382c79bdcf54d351778e0bd03e0ff
LastExceed <chris20194@googlemail.com>
2020-04-07 11:02:16 +0200

    remove item_id from OrderUpdate.kt

--- src/main/kotlin/payload/request/OrderUpdate.kt
+++ src/main/kotlin/payload/request/OrderUpdate.kt
@@ -1,12 +1,10 @@
 package payload.request
 
-import enums.IdItem
 import enums.IdOrder
 
 data class OrderUpdate(
 	val order_id: IdOrder,
 	val visible: Boolean,
-	val item_id: IdItem,
 	val platinum: Int,
 	val quantity: Int,
 	val mod_rank: Int? = null
