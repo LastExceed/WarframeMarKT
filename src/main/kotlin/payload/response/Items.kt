4615ec8bca5641486dbfc80280b9315f046c96e3
LastExceed <chris20194@googlemail.com>
2021-05-30 19:40:10 +0200

    rename Item -> ItemShort to match api doc

--- src/main/kotlin/payload/response/Items.kt
+++ src/main/kotlin/payload/response/Items.kt
@@ -6,9 +6,9 @@ import enums.ResourceLocation
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Items private constructor(val items: List<Item>) {
+data class Items private constructor(val items: List<ItemShort>) {
 	@Serializable
-	data class Item private constructor(
+	data class ItemShort private constructor(
 		val url_name: ItemUrlName,
 		val id: IdItem,
 		val item_name: String,
