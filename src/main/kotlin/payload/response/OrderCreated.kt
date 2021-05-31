4b4d65d34f1f2ab6c1f62ce8db5f7d00831a1c89
LastExceed <chris20194@googlemail.com>
2021-05-31 11:10:57 +0200

    move TODO to GitHub issue #8

--- src/main/kotlin/payload/response/OrderCreated.kt
+++ src/main/kotlin/payload/response/OrderCreated.kt
@@ -4,4 +4,4 @@ import payload.response.common.Order
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class OrderCreated private constructor(val order: Order) //TODO: deduplicate (created/updated/deleted)
+data class OrderCreated private constructor(val order: Order)
