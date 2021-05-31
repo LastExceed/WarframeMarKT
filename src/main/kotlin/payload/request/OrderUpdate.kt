17fe3cd059e67046e7abb25be19c9cd22a8c17b2
LastExceed <chris20194@googlemail.com>
2021-05-31 10:53:07 +0200

    move TODO to GitHub issue #8

--- src/main/kotlin/payload/request/OrderUpdate.kt
+++ src/main/kotlin/payload/request/OrderUpdate.kt
@@ -4,7 +4,7 @@ import enums.IdOrder
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class OrderUpdate(//TODO: inherit from OrderCreate or sth
+data class OrderUpdate(
 	val order_id: IdOrder, //we already need to specify it in the url, but also have to put it here again. KycKyc pls
 	val visible: Boolean,
 	val platinum: Int,
