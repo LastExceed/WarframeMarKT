0a39a6311187597f7136ed199f0ca532ae1647d9
LastExceed <chris20194@googlemail.com>
2021-06-08 12:30:44 +0200

    split ItemFull

--- src/main/kotlin/payload/response/common/Item.kt
+++ src/main/kotlin/payload/response/common/Item.kt
@@ -15,6 +15,33 @@ sealed class ItemNamed : Item() {
 }
 
 @Serializable
+sealed class ItemData : ItemNamed() {
+	abstract val ducats: Int? //prime stuff only
+	abstract val icon: ResourceLocation
+	abstract val icon_format: String//enum?
+	abstract val mod_max_rank: Int? //mods only
+	abstract val subtypes: List<String>? //eg relic refinement - enum ?
+	abstract val sub_icon: ResourceLocation?
+	abstract val tags: List<String>
+	abstract val de: Lang
+	abstract val en: Lang
+	abstract val es: Lang
+	abstract val fr: Lang
+	abstract val ko: Lang
+	abstract val pl: Lang
+	abstract val pt: Lang
+	abstract val ru: Lang
+	abstract val sv: Lang
+	abstract val `zh-hant`: Lang
+	abstract val `zh-hans`: Lang
+
+	@Serializable
+	sealed class Lang protected constructor() {
+		abstract val item_name: String
+	}
+}
+
+@Serializable
 data class ItemDescriptor private constructor(
 	override val id: IdItem,
 	val items_in_set: List<ItemFull>
@@ -29,43 +56,74 @@ data class ItemShort private constructor(
 ) : ItemNamed()
 
 @Serializable
+open class ItemInOrder protected constructor(
+	override val id: IdItem,
+	override val thumb: ResourceLocation,
+	override val url_name: ItemUrlName,
+	override val ducats: Int? = null,
+	override val icon: ResourceLocation,
+	override val icon_format: String,
+	override val mod_max_rank: Int? = null,
+	override val subtypes: List<String>? = null,
+	override val sub_icon: ResourceLocation?,
+	override val tags: List<String>,
+	override val de: Lang,
+	override val en: Lang,
+	override val es: Lang,
+	override val fr: Lang,
+	override val ko: Lang,
+	override val pl: Lang,
+	override val pt: Lang,
+	override val ru: Lang,
+	override val sv: Lang,
+	override val `zh-hant`: Lang,
+	override val `zh-hans`: Lang,
+) : ItemData() {
+	@Serializable
+	data class Lang private constructor(
+		override val item_name: String,
+	) : ItemData.Lang()
+}
+
+@Serializable
 data class ItemFull private constructor(
 	override val id: IdItem,
 	override val thumb: ResourceLocation,
 	override val url_name: ItemUrlName,
-	val ducats: Int? = null, //prime stuff only
-	val icon: ResourceLocation,
-	val icon_format: String,//enum?
-	val mod_max_rank: Int? = null, //mods only
-	val subtypes: List<String>? = null, //for relic refinement - enum ?
-	val sub_icon: ResourceLocation?,
-	val tags: List<String>,
-	val de: Lang,
-	val en: Lang,
-	val es: Lang,
-	val fr: Lang,
-	val ko: Lang,
-	val pl: Lang,
-	val pt: Lang,
-	val ru: Lang,
-	val sv: Lang,
-	val `zh-hant`: Lang,
-	val `zh-hans`: Lang,
+	override val ducats: Int? = null, //prime stuff only
+	override val icon: ResourceLocation,
+	override val icon_format: String,//enum?
+	override val mod_max_rank: Int? = null, //mods only
+	override val subtypes: List<String>? = null, //for relic refinement - enum ?
+	override val sub_icon: ResourceLocation?,
+	override val tags: List<String>,
+	override val de: Lang,
+	override val en: Lang,
+	override val es: Lang,
+	override val fr: Lang,
+	override val ko: Lang,
+	override val pl: Lang,
+	override val pt: Lang,
+	override val ru: Lang,
+	override val sv: Lang,
+	override val `zh-hant`: Lang,
+	override val `zh-hans`: Lang,
+
 	val mastery_level: Int? = null,
 	//set_root
-	val rarity: Rarity? = null, //missing in OrderCreate
-	val trading_tax: Int? = null //missing in OrderCreate
-) : ItemNamed() {
+	val rarity: Rarity? = null,
+	val trading_tax: Int
+) : ItemData() {
 	@Serializable
 	data class Lang private constructor(
-		val item_name: String, //OrderCreate only has this
-		val wiki_link: String? = null,
-		val description: String? = null,
-		val drop: List<Drop>? = null,
+		override val item_name: String,
+		val wiki_link: String,
+		val description: String,
+		val drop: List<Drop>,
 		val codex: String? = null, //in case of incomplete localization
 		val thumb: ResourceLocation? = null, //in case of incomplete localization
 		val icon: ResourceLocation? = null //in case of incomplete localization
-	) {
+	) : ItemData.Lang() {
 		@Serializable
 		data class Drop private constructor(
 			val name: String,
