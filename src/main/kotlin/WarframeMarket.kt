d5ba862fbea05e06893db619cfc16b20b2037df1
LastExceed <chris20194@googlemail.com>
2021-06-08 19:10:47 +0200

    replace OrderUpdated with OrderCreated

--- src/main/kotlin/WarframeMarket.kt
+++ src/main/kotlin/WarframeMarket.kt
@@ -175,9 +175,9 @@ object WarframeMarket : Endpoint(null) {
 				override suspend fun get() = requestUnwrapped<OwnOrders>(HttpMethod.Get)
 				override suspend fun create(payload: OrderCreate) = requestUnwrapped<OrderCreated>(HttpMethod.Post, payload)
 
-				class ORDER(order_id: String) : Endpoint(orders), Update<OrderUpdate, OrderUpdated>, Delete<OrderDeleted> {
+				class ORDER(order_id: String) : Endpoint(orders), Update<OrderUpdate, OrderCreated>, Delete<OrderDeleted> {
 					override val pathName = order_id
-					override suspend fun update(payload: OrderUpdate) = requestUnwrapped<OrderUpdated>(HttpMethod.Put, payload)
+					override suspend fun update(payload: OrderUpdate) = requestUnwrapped<OrderCreated>(HttpMethod.Put, payload)
 					override suspend fun delete() = requestUnwrapped<OrderDeleted>(HttpMethod.Delete)
 					suspend fun close() = requestUnwrapped<OrderClosed>(HttpMethod.Put, url = orders.url + "/close/" + pathName)
 				}
