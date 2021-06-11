b8527fe79cbe27a37d0c76927fd7884e0f9f7a98
LastExceed <chris20194@googlemail.com>
2021-06-11 10:00:22 +0200

    add workaround for kotlinx.serialization bug

--- src/main/kotlin/enums/ReviewType.kt
+++ src/main/kotlin/enums/ReviewType.kt
@@ -8,7 +8,15 @@ import kotlinx.serialization.encoding.*
 @Serializable(ReviewTypeAsIntSerializer::class)
 enum class ReviewType(val serializedForm: Int) {
 	Review(1),
-	Report(-1)
+	Report(-1);
+
+	//workaround for https://github.com/Kotlin/kotlinx.serialization/issues/1386
+	//pretty hacky as the docs explicitly state that this doesn't work, but it does (for now)
+	//only matters for serializing a review type DIRECTLY (which is never required for interfacing with the wf.m api anyway)
+	//this gets fixed in the next version of the kotlin compiler so it's not worth the time looking for a better workaround
+	companion object {
+		fun serializer() = ReviewTypeAsIntSerializer
+	}
 }
 
 object ReviewTypeAsIntSerializer : KSerializer<ReviewType> {
