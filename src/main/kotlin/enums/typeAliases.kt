b06b8f18f238cb87eeb870dab5bda5c4d2c022e8
LastExceed <chris20194@googlemail.com>
2021-06-11 14:03:42 +0200

    use generic value class for ids

--- src/main/kotlin/enums/typeAliases.kt
+++ src/main/kotlin/enums/typeAliases.kt
@@ -1,17 +1,26 @@
 package enums
 
-typealias IdAuction = String
-typealias IdBid = String
-typealias IdBan = String
-typealias IdChat = String
-typealias IdDucats = String
-typealias IdItem = String
-typealias IdMessage = String
-typealias IdOrder = String
-typealias IdReview = String
-typealias IdSnapshot = String
-typealias IdUser = String
-typealias IdRivenItem = String
-typealias IdRivenAttribute = String
+import kotlinx.serialization.Serializable
+import payload.response.*
+import payload.response.common.*
+
+typealias IdAuction = Id<Auction>
+typealias IdBid = Id<Bids.Bid>
+typealias IdBan = Id<Bans.Ban>
+typealias IdChat = Id<Chats.Chat>
+typealias IdDucats = Id<Ducats>
+typealias IdItem = Id<Item>
+typealias IdMessage = Id<Chats.Chat.Message>
+typealias IdOrder = Id<Order>
+typealias IdReview = Id<Review>
+typealias IdSnapshot = Id<ItemStatistics.Statistics.Snapshot>
+typealias IdUser = Id<User>
+typealias IdRivenItem = Id<RivenItems.RivenItem>
+typealias IdRivenAttribute = Id<RivenAttributes.RivenAttribute>
+
 typealias ItemUrlName = String
 typealias ResourceLocation = String
+
+@JvmInline
+@Serializable
+value class Id<T>(val value: String)
