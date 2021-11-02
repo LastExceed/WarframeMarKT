205aea6bea5ee9fa2a21ceb322f564c481de1087
LastExceed <chris20194@googlemail.com>
2021-11-02 12:15:40 +0100

    revert 'move Id class to separate file'

--- src/main/kotlin/Id.kt
+++
@@ -1,9 +0,0 @@
-import kotlinx.serialization.Serializable
-
-interface IdCarrier {
-	val id: Id<out IdCarrier> //kotlin doesn't have a Self type so this is the best we can do
-}
-
-@JvmInline
-@Serializable
-value class Id<T : IdCarrier>(val value: String)
--- src/main/kotlin/enums/typeAliases.kt
+++ src/main/kotlin/enums/typeAliases.kt
@@ -1,4 +1,14 @@
 package enums
 
+import kotlinx.serialization.Serializable
+
 typealias ItemUrlName = String
 typealias ResourceLocation = String
+
+interface IdCarrier {
+	val id: Id<out IdCarrier> //kotlin doesn't have a Self type so this is the best we can do
+}
+
+@JvmInline
+@Serializable
+value class Id<T : IdCarrier>(val value: String)
